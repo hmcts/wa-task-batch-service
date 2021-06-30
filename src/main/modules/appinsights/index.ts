a6b800e40eefdf0f7e2216742fb91185fae0e5d7
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2021-06-30 14:05:16 +0100

    Clean config

--- src/main/modules/appinsights/index.ts
+++ src/main/modules/appinsights/index.ts
@@ -1,12 +1,15 @@
 import config from 'config';
 
 const appInsights = require('applicationinsights');
+const appInsightsEnabled: boolean = config.get('appInsights.enabled');
+const appInsightsKey: string = config.get('appInsights.instrumentationKey');
 
 export class AppInsights {
 
   enable(): void {
-    if (config.get('appInsights.instrumentationKey')) {
-      appInsights.setup(config.get('appInsights.instrumentationKey'))
+    console.info(appInsightsEnabled)
+    if (appInsightsEnabled && appInsightsEnabled === true) {
+      appInsights.setup(appInsightsKey)
         .setSendLiveMetrics(true)
         .start();
 
