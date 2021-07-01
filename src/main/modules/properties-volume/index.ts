295ece3a3f16b05a90bd5932d1d11c00f84ac591
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2021-07-01 16:58:07 +0100

    fix secret path

--- src/main/modules/properties-volume/index.ts
+++ src/main/modules/properties-volume/index.ts
@@ -9,7 +9,7 @@ export class PropertiesVolume {
     if (server.locals.ENV !== 'development') {
       propertiesVolume.addTo(config);
 
-      set(config, 'appInsights.instrumentationKey', get(config, 'secrets.fact.AppInsightsInstrumentationKey'));
+      set(config, 'appInsights.instrumentationKey', get(config, 'secrets.wa.AppInsightsInstrumentationKey'));
       set(config, 's2s.secret', get(config, 'secrets.wa.s2s-secret-task-monitor'));
     }
   }
