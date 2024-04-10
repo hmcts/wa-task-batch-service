49c300dabc2eef567a75f60360e92859f9afb5b4
Sammi Chong <shinyee.chong@hmcts.net>
2024-04-10 18:41:24 +0100

    Update

--- eslint.config.js
+++ eslint.config.js
@@ -20,11 +20,11 @@ export default [
       }
     },
     rules: {
-      indent: ['error', 2, { 'SwitchCase': 1 }],
-      linebreak-style: ['error', 'unix'],
-      quotes: ['error', 'single'],
-      comma-dangle: ['error', 'always-multiline'],
-      semi: ['error', 'always'],
+      'indent': ['error', 2, { 'SwitchCase': 1 }],
+      'linebreak-style': ['error', 'unix'],
+      'quotes': ['error', 'single'],
+      'comma-dangle': ['error', 'always-multiline'],
+      'semi': ['error', 'always'],
     },
   },
   {
