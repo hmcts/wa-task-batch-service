e27faf8da14c2c552aeb3f4419f785271d064659
Sammi Chong <shinyee.chong@hmcts.net>
2024-04-17 16:34:04 +0100

    Update

--- jest.config.js
+++ jest.config.js
@@ -10,4 +10,5 @@ module.exports = {
     '^.+\\.ts?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: ['/node_modules/(?!react-dnd|dnd-core|@react-dnd|react-dnd-html5-backend)'],
 }
