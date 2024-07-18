8b8df11902376e214d4a5c3832390c610ac6ca3c
skandala-cgi <sita.kandala@hmcts.net>
2024-07-18 10:51:17 +0100

    RWA-3338 add smoke test phase -- only for testing

---
+++ src/test/smoketest/smoke.test.ts
@@ -0,0 +1,26 @@
+const { spawn } = require('child_process');
+
+test('should check if server process is running', async () => {
+  const serverProcess = spawn('yarn', ['start:dev'], { shell: true, stdio: 'pipe' });
+
+  let output = '';
+
+  serverProcess.stdout.on('data', (data: Buffer) => {
+    output += data.toString();
+  });
+
+  serverProcess.stderr.on('data', (data: Buffer) => {
+    output += data.toString();
+});
+
+  // Wait for some time to let the server start
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
+
+  console.log('Output log', output);
+  expect(output).toContain('Application started');
+
+  // Clean up the server process
+  serverProcess.kill();
+});
+
