9e5851fd76c412d2908310b5d217d74728cb9d1e
skandala-cgi <sita.kandala@hmcts.net>
2024-07-18 14:20:44 +0100

    RWA-3338 add smoke test phase -- only for testing

--- src/test/smoketest/smoke.test.ts
+++ src/test/smoketest/smoke.test.ts
@@ -21,7 +21,7 @@ test('should check if server process is running', async () => {
   });
 
   // Wait for some time to let the server start
-  await new Promise((resolve) => setTimeout(resolve, 3000));
+  await new Promise((resolve) => setTimeout(resolve, 60000));
 
   console.log('Output log', output);
   expect(output).toContain('Application started');
