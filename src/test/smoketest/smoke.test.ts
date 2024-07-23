e8e4d4c13640dc16c3a080129f16b7160ab2b139
skandala-cgi <66466864+skandala-cgi@users.noreply.github.com>
2024-07-23 15:58:27 +0100

    RWA-3338 add smoke test phase (#410)
    * RWA-3338 add smoke test phase
    
    * RWA-3338 add smoke test phase
    
    * RWA-3338 add smoke test phase
    
    * RWA-3338 add smoke test phase
    
    * RWA-3338 add smoke test phase
    
    * RWA-3338 add smoke test phase
    
    * RWA-3338 add smoke test phase
    
    * RWA-3338 add smoke test phase
    
    * RWA-3338 add smoke test phase
    
    * RWA-3338 add smoke test phase
    
    * RWA-3338 add smoke test phase
    
    * RWA-3338 add smoke test phase
    
    * RWA-3338 add smoke test phase
    
    * RWA-3338 add smoke test phase
    
    * RWA-3338 add smoke test phase -- only for testing
    
    * RWA-3338 add smoke test phase -- only for testing
    
    * RWA-3338 add smoke test phase -- only for testing
    
    * RWA-3338 add smoke test phase -- only for testing
    
    * RWA-3338 add smoke test phase -- only for testing
    
    * RWA-3338 add smoke test phase -- only for testing
    
    * RWA-3338 add smoke test phase -- only for testing
    
    * RWA-3338 add smoke test phase -- only for testing
    
    * RWA-3338 add smoke test phase -- only for testing
    
    * RWA-3338 add smoke test phase -- only for testing
    
    * RWA-3338 add smoke test phase -- only for testing
    
    * RWA-3338 add smoke test phase -- only for testing
    
    * RWA-3338 add smoke test phase -- only for testing
    
    * RWA-3338 add smoke test phase -- only for testing
    
    * RWA-3338 add smoke test phase -- only for testing
    
    * RWA-3338 add smoke test phase -- only for testing
    
    * RWA-3338 add smoke test phase -- only for testing
    
    * RWA-3338 add smoke test phase -- only for testing
    
    * RWA-3338 add smoke test phase -- only for testing
    
    * RWA-3338 add smoke test phase -- only for testing

---
+++ src/test/smoketest/smoke.test.ts
@@ -0,0 +1,37 @@
+import { spawn, ChildProcess } from 'child_process';
+let serverProcess: ChildProcess;
+
+beforeAll(() => {
+  console.log('Starting the server process', process.env.NODE_ENV);
+
+  serverProcess = spawn('yarn', ['start'], {
+    shell: true,
+    stdio: 'pipe',
+    env: { ...process.env, ALLOW_CONFIG_MUTATIONS: true.toString()},
+  });
+});
+
+afterAll(() => {
+  // Clean up the server process
+  if (serverProcess) {
+    serverProcess.kill('SIGINT');
+  }
+});
+
+test('should check if server process is running', async () => {
+  let output = '';
+
+  serverProcess.stdout.on('data', (data: Buffer) => {
+    output += data.toString();
+  });
+
+  serverProcess.stderr.on('data', (data: Buffer) => {
+    output += data.toString();
+  });
+
+  // Wait for some time to let the server start
+  await new Promise((resolve) => setTimeout(resolve, 30000));
+
+  console.log('Output log', output);
+  expect(output).toContain('Application started');
+}, 60000);
