a41fc68affddb6d109032e530f036f8caf650137
Martin <martin.spasov@hmcts.net>
2026-01-22 13:24:44 +0000

    test

--- src/test/functional/startup-functional.test.ts
+++ src/test/functional/startup-functional.test.ts
@@ -1,58 +1,40 @@
-import axios, {AxiosInstance} from 'axios';
+// startup-functional.test.ts
 import {spawn, ChildProcess} from 'child_process';
-import S2SService from '../../main/services/s2s-service';
-// import config from 'config';
+
 let serverProcess: ChildProcess;
 
-// const BASE_URL: string = config.get('services.taskMonitor.url');
-const BASE_URL: string = 'http://wa-task-monitor-aat.service.core-compute-aat.internal';
-const JOB_REQUEST = {job_details: {name: 'INITIATION'}};
-const s2sService: S2SService = S2SService.getInstance();
-
-const taskMonitorApi: AxiosInstance = axios.create({
-  baseURL: BASE_URL,
-  timeout: 30000,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
-
 beforeAll(() => {
-  console.log('Starting the server process', process.env.NODE_ENV);
+  console.log('Starting the server process');
 
   serverProcess = spawn('yarn', ['start'], {
     shell: true,
     stdio: 'pipe',
-    env: { ...process.env, ALLOW_CONFIG_MUTATIONS: true.toString()},
+    env: {...process.env, ALLOW_CONFIG_MUTATIONS: 'true'},
   });
 });
 
 afterAll(() => {
-  // Clean up the server process
   if (serverProcess) {
     serverProcess.kill('SIGINT');
   }
 });
 
-test('should check if server process is running', async () => {
+test('should verify server starts, gets S2S token, and calls task monitor', async () => {
   let output = '';
 
-  serverProcess.stdout.on('data', (data: Buffer) => {
+  serverProcess.stdout?.on('data', (data: Buffer) => {
     output += data.toString();
   });
 
-  serverProcess.stderr.on('data', (data: Buffer) => {
+  serverProcess.stderr?.on('data', (data: Buffer) => {
     output += data.toString();
   });
 
-  // Wait for some time to let the server start
+  // Wait for the server to start and complete its job
   await new Promise((resolve) => setTimeout(resolve, 30000));
 
-  const s2sToken = await s2sService.getServiceToken();
-  const headers = {ServiceAuthorization: s2sToken};
-  await taskMonitorApi.post('/monitor/tasks/jobs', JOB_REQUEST, {headers});
-
   console.log('Output log', output);
+
   expect(output).toContain('Application started');
   expect(output).toContain('Received S2S token');
   expect(output).toContain('Status: 200');
