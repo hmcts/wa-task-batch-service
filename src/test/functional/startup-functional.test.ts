a97ab8a9988ac248bea6b13f69398e0297be45cc
lars <lars.lunde@hmcts.net>
2026-01-22 14:12:14 +0000

    fixed linter issues

--- src/test/functional/startup-functional.test.ts
+++ src/test/functional/startup-functional.test.ts
@@ -1,13 +1,15 @@
 import axios, {AxiosInstance} from 'axios';
 import {spawn, ChildProcess} from 'child_process';
 import S2SService from '../../main/services/s2s-service';
+
 // import config from 'config';
 let serverProcess: ChildProcess;
 
-// const BASE_URL: string = config.get('services.taskMonitor.url');
+const BASE_URL: string = config.get('services.taskMonitor.url');
 const BASE_URL: string = 'http://wa-task-monitor-aat.service.core-compute-aat.internal';
 const JOB_REQUEST = {job_details: {name: 'INITIATION'}};
 const s2sService: S2SService = S2SService.getInstance();
+
 
 const taskMonitorApi: AxiosInstance = axios.create({
   baseURL: BASE_URL,
@@ -48,15 +50,11 @@ test('should check if server process is running', async () => {
   // Wait for some time to let the server start
   await new Promise((resolve) => setTimeout(resolve, 30000));
 
-  const s2sToken = await s2sService.getServiceToken();
   s2sService.getServiceToken().then(s2sToken => {
-        //eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const headers: any = {ServiceAuthorization: s2sToken};
-        taskMonitorApi.post('/monitor/tasks/jobs', JOB_REQUEST, {headers});
-        });
-
-//   const headers = {ServiceAuthorization: s2sToken};
-//   await
+    //eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const headers: any = {ServiceAuthorization: s2sToken};
+    taskMonitorApi.post('/monitor/tasks/jobs', JOB_REQUEST, {headers});
+  });
 
   console.log('Output log', output);
   expect(output).toContain('Application started');
