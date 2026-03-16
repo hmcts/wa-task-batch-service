3ea811f185ea26a032759fc7b2abcb54f9e0f112
Aditya Dwadasi <aditya.dwadasi@hmcts.net>
2026-03-16 15:59:45 +0000

    Removed certificate related code.

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-import * as https from 'https';
 import {app} from './app';
 import {TaskMonitorService} from './services/task-monitor-service';
 import {exit} from './utils/exit';
@@ -10,16 +9,8 @@ const logger = Logger.getLogger('server');
 // TODO: set the right port for your application
 const port: number = parseInt(process.env.PORT, 10) || 9999;
 
-if (app.locals.ENV === 'development') {
-  const sslOptions = {};
-  const server = https.createServer(sslOptions, app);
-  server.listen(port, () => {
-    logger.info(`Application started: https://localhost:${port}`);
-  });
-} else {
-  app.listen(port, () => {
-    logger.info(`Application started: http://localhost:${port}`);
-  });
-}
+app.listen(port, () => {
+  logger.info(`Application started: http://localhost:${port}`);
+});
 
 new TaskMonitorService().createJob().then(() => exit(0));
